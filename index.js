b2f2c70fd9865f6d1d16c2d605e7da521b569561
Josh Kim <josh_kimmm@yahoo.com>
2020-03-08 18:37:44 -0700

    pushing files to github

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, resp) => {
+  console.log('incoming webhook');
+  console.log({ body: req.body, headers: req.headers }, '\n', '\n');
+
+  console.log("THIS IS FOR JOSH TO LOOK AT MANG." + req.body);
+  resp.status(200).end();
+});
+
+app.listen(3000, () => console.log('listening'));
+
+//woot dsf pullrequersadfasdfdsfasdasdasddsfdsfsdfdsfdsfdfdfd

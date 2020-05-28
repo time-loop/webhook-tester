dd8ac4e929436c7d719cc2c9d3e03d0b1dc9b390
Josh Kim <josh_kimmm@yahoo.com>
2020-05-28 14:11:37 -0700

    CU-xq9940 - Git v2 oh yes

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, resp) => {
-  console.log('incoming webhook');
-  console.log({ body: req.body, headers: req.headers }, '\n', '\n');
+  console.log(req.body.event + " trigger");
+  console.log(req.body, '\n\n');
 
-  console.log("THIS IS FOR JOSH TO LOOK AT MANG." + req.body);
   resp.status(200).end();
 });
 
-app.listen(3000, () => console.log('listening'));
+app.listen(3000, () => console.log('listening to port 3000'));
 
-//woot dsf pullrequersadfasdfdsfasdasdasddsfdsfsdfdsfdsfdfdfd
+//woot dsf quersadfasdfdsfasdasdasddsfdsfsdfdsfdsfdfdfd

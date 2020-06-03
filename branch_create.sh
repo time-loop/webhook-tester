86801392f9e87fb9de4a6c7b72aa0d2b70f25fee
Joshua Kim <josh_kimmm@yahoo.com>
2020-06-03 15:09:44 -0700

    creating script to create bunch of branches

--- branch_create.sh
+++ branch_create.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 for i in {6..50}
 do
-	"git checkout -b branch$i; git push --set-upstream origin branch$i"	
+	$(git checkout -b branch$i)
+	$(git push --set-upstream origin branch$i)
 done
